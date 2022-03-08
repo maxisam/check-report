a07df21dee20a0d8f2aa0f0db6568394683959ff
Sam Lin <maxisam@gmail.com>
2022-03-07 22:05:55 -0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import * as core from '@actions/core';
+import {inspect} from 'util';
+import {getInputs, getOctokit, getOwnerRepo} from './common';
+import {createActionRequest, DispatchEventRequest} from './create-action-request';
+
+async function run(): Promise<void> {
+  const inputs = getInputs();
+  const [owner, repo] = getOwnerRepo(inputs.owner, inputs.repository);
+  const octokit = getOctokit(inputs.authToken, 'github-action');
+  let request: DispatchEventRequest;
+
+  try {
+    request = createActionRequest(owner, repo);
+    core.debug(`dispatch event request: ${inspect(request)}`);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(`Error creating status request object: ${error.message}`);
+    }
+    return;
+  }
+
+  if (octokit === null) {
+    core.setFailed('Error creating octokit:\noctokit was null');
+  } else {
+    try {
+      await octokit.rest.repos.getViews(request);
+    } catch (error) {
+      core.debug(inspect(error));
+      if (error instanceof Error) {
+        core.setFailed(`Error setting status:\n${error.message}\nRequest object:\n${JSON.stringify(request, null, 2)}`);
+      }
+    }
+  }
+}
+
+run();
